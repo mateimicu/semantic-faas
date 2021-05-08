7dadbe1fdd42824fba3653917fdbd5e4f102b5fc
Matei-Marius Micu <micumatei@gmail.com>
2021-05-08 18:38:31 +0300

    Add tool to fetch labels for all WikiData items we use
    When working in protege with WikiData it is hard to reason about items because
    of there random names (Example `Q182933`), by fetching the proffered label for
    each item and embedding it in the ontology we get a better user experience when
    modeling.
    
    This will have no impact on SPARQL query's
    
    Example:
     From `Q182933` to `type of instruction set architecture`
    
    Co-authored-by: Stefan Gordin <stefangordindev@gmail.com>

---
+++ prefix_dbpedia_items.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Utility to parse a .owl document and add labels to all WikiData elements.
+"""
+import os
+import itertools
+
+from rdflib import Graph, Literal
+from rdflib.namespace import RDFS
+from wikidata.client import Client
+WIKIDATA_ENTITY_PREFIX = 'http://www.wikidata.org/entity/'
+
+def get_label(elem):
+    client = Client()
+    entity = client.get(elem.rsplit('/', 1)[-1], load=True)
+    return Literal(entity.label)
+
+
+def main():
+    ontology_file = "diz.owl"
+    ontology_file_output = "diz.owl"
+
+    g = Graph()
+    g.parse(ontology_file, format="n3")
+
+    # get all wikidata elements
+    wikidata_elements = []
+
+    for elem in itertools.chain(g.subjects(), g.predicates(), g.objects()):
+        if elem.startswith(WIKIDATA_ENTITY_PREFIX):
+            wikidata_elements.append(elem)
+
+
+    print(wikidata_elements)
+    # if the label is missing fetch it and add it to our items
+    for wd in wikidata_elements:
+        if g.label(wd, default=None) is None:
+            label = get_label(wd)
+            g.add([wd, RDFS.label, label])
+    g.serialize(ontology_file_output, format="turtle")
+
+if __name__ == '__main__':
+    main()
