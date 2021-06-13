0b0025cdd9cee8ffec0778899d2b8d5edfd32ca9
Matei-Marius Micu <micumatei@gmail.com>
2021-06-13 19:49:55 +0300

    Add CLI arguments to test_sparql script

--- test_sparql.py
+++ test_sparql.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 import pathlib
+import sys
+import argparse
+def get_args(args):
+    parser = argparse.ArgumentParser("test_sparql")
+    parser.add_argument(
+        "--ontology", help="Path to ontology", default="faas.owl")
+    parser.add_argument(
+        "--sparql-dir", help="Directory with spraql queryx's",
+        default="./sparql")
+    parser.add_argument(
+        "--sparql-regex-query", help="Regex to filter desired files from --sparql-dir",
+        default="*.sparql")
+
+    return parser.parse_args(args)
 
 
 from rdflib import Graph
 
-QUERY = pathlib.Path("./sparql").rglob("*.sparql")
 
 def main():
-    ontology_file = "faas.owl"
+    args = get_args(sys.argv[1:])
+    query_list = sorted(pathlib.Path(args.sparql_dir).rglob(args.sparql_regex_query))
     graph = Graph()
-    graph.parse(ontology_file, format="n3")
+    graph.parse(args.ontology, format="n3")
 
-    for query in QUERY:
+    for query in query_list:
         results = graph.query(open(query).read())
         print(query, end="\n"*2)
         for row in results:
